5f2d0d8e5186815ffdbcd8292e9acb0649ce222f
paulinavazquez25 <paulinavazquez2525@gmail.com>
2024-06-27 11:50:46 -0300

    Entrega de Paulina Vazquez - 325917

--- entrega/entrega.sh
+++ entrega/entrega.sh
@@ -1,2 +1,54 @@
-# Entrega Nombre Apellido - NumeroDeEstudiante
+# Entrega Paulina Vazquez - 325917
+#!/bin/bash
 
+while true; do
+    mostrar_menu
+    leer_opcion
+done
+
+
+mostrar_menu() {
+    echo "============================== MENU PRINCIPAL =============================="
+    
+    echo "1 - Opcion 1"
+    echo "2 - Opcion 2"
+    echo "3 - Opcion 3"
+    echo "4 - Opcion 4"
+    echo "0 - Salir"
+
+}
+
+leer_opcion() {
+    read -p "Seleccione opcion del menu: " opcion
+    case $opcion in 
+        1) opcion_1
+        ;;
+        2) opcion_2
+        ;;
+        3) opcion_3
+        ;;
+        4) opcion_4
+        ;;
+        0) salir
+        ;;
+        *) echo "Vuelva a Intentar"
+            leer_opcion
+    esac        
+}
+
+opcion_1(){
+    echo "Elegiste opcion 1"
+}
+opcion_2(){
+    echo "Elegiste opcion 2"
+}
+opcion_3(){
+    echo "Elegiste opcion 3"
+}
+opcion_4(){
+    echo "Elegiste opcion 4"
+}
+
+salir(){
+    exit
+}
